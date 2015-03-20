3d220569f926e13519d30b0a6ec82bc462274aba
bmitchell287 <bmitchell287@users.noreply.github.com>
2015-03-20 15:45:48 -0700

    Update main.m
    removed commenting

--- Sample/O365-iOS-Snippets/main.m
+++ Sample/O365-iOS-Snippets/main.m
@@ -2,9 +2,7 @@
 //  main.m
 //  O365-iOS-Snippets
 //
-//  Created by Brendan Mitchell on 2/12/15.
-//  Copyright (c) 2015 Microsoft. All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
